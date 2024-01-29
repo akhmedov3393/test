427be9734f9e6fd984c6e2eb56011970f90b9540
akhmedov3393 <akhmedov3393@gmail.com>
2024-01-29 13:31:10 +0500

    masala

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+// 1-masala Tomoni x bo'lgan kubning hajmini toping
+//let x= +prompt("X ni kirit:::")
+
+//console.log(x**3);
+
+
+
+// 2-masala. Radiuslari r1, r2, r3 bo’lgan 3 to doira radiuslari berilgan. Doiralarni yuzlarini hisoblang.
+//let r1 = prompt("R1 ni kirit:::")
+//let r2 = prompt("R2 ni kirit:::")
+//let r3 = prompt("R3 ni kirit:::")
+
+//console.log(Math.PI*r1**2 .toFixed(2) , Math.PI*r2**2 .toFixed(2), Math.PI*r3**2 .toFixed(2));
+
+
+// 3-masala. Yuzasi s va balandligi h bo`lgan uch burchak berilgan. Uni asosini toping.
+
+//let s= prompt("S ni kirit:::")
+//let h= prompt("H ni kirit:::")
+//
+//console.log(2*s/h);
+
+// 4-maala. Radiusi r bo`lgan sharning yuzini toping.
+//let r = prompt("R ni kirit:::")
+//console.log(4*Math.PI*r**2 .toFixed(2) )
+
+
+// 5-masala. a,b va c tamonli uch burchak berilgan. Uchburchakning yarim perimetri topilsin.
+let a = +prompt("a ni kirit:::")
+let b = +prompt("b ni kirit:::")
+let c = +prompt("c ni kirit:::")
+let p = a+b+c
+console.log(p/2);
